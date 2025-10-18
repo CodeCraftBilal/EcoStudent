f264584528ac368086956e3950f4281cce925f38
Muhammad Bilal Khan <bilalkhan751150@gmail.com>
2025-10-18 10:30:35 +0500

    users module initialized

---
+++ apps/backend/src/users/dto/create-user.dto.ts
@@ -0,0 +1,45 @@
+import { IsDecimal, IsEmail, IsNumber, IsOptional, IsString, Matches, MaxLength, min, MinLength, minLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(3)
+  @MaxLength(255)
+  user_name: string;
+
+  @IsString()
+  @MinLength(8)
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    {
+      message:
+        'Password too weak. It must contain at least 8 characters, one uppercase, one lowercase, one number, and one special character.',
+    },
+  )
+  password: string;
+
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsNumber()
+  @MaxLength(20)
+  @MinLength(11)
+  phone_number: string;
+
+  @IsString()
+  profile_picture: string;
+
+  @IsString() 
+  @MinLength(5)
+  @MaxLength(255)
+  user_location: string;
+
+  @IsDecimal()
+  @IsOptional()
+  latitude: number;
+
+  @IsDecimal()
+  @IsOptional()
+  longitude: number;
+
+}
