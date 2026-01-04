fd46f990fbb7c022327aaf6bdd87c81d009056e7
Muhammad Bilal Khan <bilalkhan751150@gmail.com>
2026-01-04 08:32:08 +0500

    configured shadcn ui

--- apps/frontend/app/test/page.tsx
+++ apps/frontend/app/test/page.tsx
@@ -1,30 +1,13 @@
 'use client'
+import { Button } from '@/components/ui/button'
 import { useSocket } from '@/context/useSocket'
 import React, { useEffect, useState } from 'react'
 
 const page = () => {
-    const socket = useSocket();
-    const [message, setMessage] = useState('')
-    const handleClick = () => {
-        if(!socket) return;
-        console.log("Button clicked");
-        socket.emit("message:send", {content: "Hello from client"})
-    }
-
-    useEffect(() => {
-      socket?.on("message:send", (data:any) => {
-        console.log("Message received from server:", data);
-        setMessage(data.status.content)
-      })
-      return () => {
-        socket?.off("message:send");
-      }
-    }, [socket, message])
     
   return (
     <div>
-        <div>{message}</div>
-    <button onClick={handleClick}>Click Me</button>  
+        <Button className='m-10'>Hello</Button>  
     </div>
   )
 }
