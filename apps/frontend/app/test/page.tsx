26a8a592e7632640ae087d9cb8c11253b27a8208
Muhammad Bilal Khan <bilalkhan751150@gmail.com>
2026-01-04 09:58:27 +0500

    configured mapcn

--- apps/frontend/app/test/page.tsx
+++ apps/frontend/app/test/page.tsx
@@ -1,15 +1,23 @@
-'use client'
-import { Button } from '@/components/ui/button'
-import { useSocket } from '@/context/useSocket'
-import React, { useEffect, useState } from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Map,
+  MapControls,
+  MapMarker,
+  MarkerContent,
+  MarkerLabel,
+} from "@/components/ui/map";
+import { useSocket } from "@/context/useSocket";
+import React, { useEffect, useState } from "react";
 
 const page = () => {
-    
   return (
-    <div>
-        <Button className='m-10'>Hello</Button>  
+    <div className="w-96 h-96">
+      <Map center={[-74.006, 40.7128]} zoom={11}>
+        <MapControls />
+      </Map>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
