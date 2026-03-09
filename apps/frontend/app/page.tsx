dff024569d31b56f202a795ce3bf4e714a2858dc
Muhammad Bilal Khan <bilalkhan751150@gmail.com>
2026-03-09 16:24:12 +0500

    made home page a server component

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,43 +1,12 @@
-'use client';
-
 import Footer from '@/components/Footer';
 import { Home } from '@/components/Home';
-import { BACKEND_URL, FRONTEND_URL } from '@/lib/constants';
-import { useSearchParams } from 'next/navigation';
-import { Suspense, useEffect } from 'react';
 
-function HomePageContent() {
-  const searchParams = useSearchParams();
-  const search = searchParams.get('mode')
-  console.log(search); 
+export default function HomePage() {
 
-  useEffect(() => {
-    const test = async () => {
-      const res = await fetch(`/api/test`, {
-        method: 'POST',
-        body: JSON.stringify({
-          backendUrl: BACKEND_URL
-        })
-      })
-
-    }
-
-    test();
-
-  }, [])
-  
   return (
     <div>
       <Home />
       <Footer />
     </div>
-  );
-}
-
-export default function HomePage() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <HomePageContent />
-    </Suspense>
   );
 }
