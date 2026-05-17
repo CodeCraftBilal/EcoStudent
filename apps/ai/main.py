58b91d7421e0044b103b7d3b57cea5b67ddf62b2
Muhammad Bilal Khan <bilalkhan751150@gmail.com>
2026-05-17 14:11:14 +0500

    removed pycache from tracking

--- apps/ai/main.py
+++ apps/ai/main.py
@@ -1,15 +1,17 @@
 from fastapi import FastAPI
 from src.routes import router
-from db import db
+# from db import db
 
 app = FastAPI()
 
 @app.on_event("startup")
 async def startup():
-    await db.connect()
+    pass
+    # await db.connect()
 
 @app.on_event("shutdown")
 async def shutdown():
-    await db.disconnect()
+    pass
+    # await db.disconnect()
 
 app.include_router(router)
